1bad02b34d382c6508f53ae99022742f926dba5d
korLethean <hdgwon@naver.com>
2018-06-19 15:18:04 +0900

    minor fix

--- app/src/main/java/com/test/lethean/passwordtest/MainActivity.java
+++ app/src/main/java/com/test/lethean/passwordtest/MainActivity.java
@@ -1,16 +1,15 @@
 package com.test.lethean.passwordtest;
 
-import android.appwidget.AppWidgetManager;
-import android.content.Context;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,6 +18,8 @@ import butterknife.Unbinder;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.text_output)
     TextView textPassword;
+    @BindView(R.id.text_result)
+    TextView textMessage;
     @BindView(R.id.button_delete)
     Button buttonDelete;
     @BindView(R.id.button_clear)
@@ -43,10 +44,15 @@ public class MainActivity extends AppCompatActivity {
     Button button9;
     @BindView(R.id.button_zero)
     Button button0;
+    @BindView(R.id.layout_panel)
+    LinearLayout layoutPanel;
     Unbinder unbinder;
 
     int deviceWidth;
     int deviceHeight;
+
+    String PASSWORD = "1234";
+    ArrayList<String> num = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,12 +67,18 @@ public class MainActivity extends AppCompatActivity {
         deviceWidth = p.x;
         deviceHeight = p.y;
 
+        for(int i = 0; i < 10 ; i++) {
+            num.add(String.valueOf(i));
+        }
+//        buttonReset();
+
         View.OnClickListener buttonListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Button button = (Button)view;
 
                 textPassword.setText(textPassword.getText().toString() + button.getText().toString());
+                screenReset();
             }
         };
         button1.setOnClickListener(buttonListener);
@@ -93,11 +105,45 @@ public class MainActivity extends AppCompatActivity {
         unbinder.unbind();
     }
 
+/*
+    private void buttonReset() {
+        int index = 1;
+        button1.setText(num.get(index++));
+        button2.setText(num.get(index++));
+        button3.setText(num.get(index++));
+        button4.setText(num.get(index++));
+        button5.setText(num.get(index++));
+        button6.setText(num.get(index++));
+        button7.setText(num.get(index++));
+        button8.setText(num.get(index++));
+        button9.setText(num.get(index++));
+        button0.setText(num.get(index++));
+    }
+*/
 
     private void screenReset() {
         if(textPassword.getText().length() == 4) {
+/*
             int x, y;
+            Random rand = new Random();
             textPassword.setText("");
+
+            x = rand.nextInt(deviceWidth) - 180;
+            y = rand.nextInt(deviceHeight) - 240;
+
+            if(x < 0)
+                x = 0;
+            if(y < 0)
+                y = 0;
+
+            layoutPanel.setPadding(x, y, 0, 0);
+*/
+            if(textPassword.getText().equals(PASSWORD)) {
+                textMessage.setText("correct password");
+            }
+            else {
+                textMessage.setText("wrong password");
+            }
         }
     }
 }
